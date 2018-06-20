3469d62ae28d6099e29cdb73ec2c136063734b8c
dkrinke <krinkoid@gmail.com>
2018-06-20 04:54:31 -0400

    Updated script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker ps
 docker stop supermarket-api
-docker run -d -p 8080:8080 dkrinke/supermarketapi:latest
+docker run -d -p 8080:8080 --name supermarket-api dkrinke/supermarketapi:latest

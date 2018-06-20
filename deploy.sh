c1d94c541b746402b6e892ae9a25769a68883689
dkrinke <krinkoid@gmail.com>
2018-06-20 05:11:48 -0400

    Updated deployment stage

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker ps
+docker pull dkrinke/supermarketapi:latest
 docker stop supermarket-api
 docker rm supermarket-api
 docker run -d -p 8080:8080 --name supermarket-api dkrinke/supermarketapi:latest

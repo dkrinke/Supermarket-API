c19fe1428c7697529a078d6868e68ad623d727ea
dkrinke <krinkoid@gmail.com>
2018-06-23 04:27:55 -0400

    Added comments

--- src/supermarketDB/db.go
+++ src/supermarketDB/db.go
@@ -1,29 +1,29 @@
 package db
 
 import (
-	"supermarketProduce"
-	"sync"
+	"supermarketProduce" //Provides the Produce Struct
+	"sync"               //Provides basic synchronization primitives such as mutual exclusion locks
 )
 
 var (
-	// db instantiated with default data
+	//db instantiated with default data
 	database = []produce.Produce{
-		produce.Produce{
+		produce.Produce{ //Required Lettuce
 			Name:  "Lettuce",
 			Code:  "A12T-4GH7-QPL9-3N4M",
 			Price: "$3.46",
 		},
-		produce.Produce{
+		produce.Produce{ //Required Peach
 			Name:  "Peach",
 			Code:  "E5T6-9UI3-TH15-QR88",
 			Price: "$2.99",
 		},
-		produce.Produce{
+		produce.Produce{ //Required Green Pepper
 			Name:  "Green Pepper",
 			Code:  "YRT6-72AS-K736-L4AR",
 			Price: "$0.79",
 		},
-		produce.Produce{
+		produce.Produce{ //Required Gala Apple
 			Name:  "Gala Apple",
 			Code:  "TQ4C-VV6T-75ZX-1RMR",
 			Price: "$3.59",
@@ -31,20 +31,20 @@ var (
 	}
 )
 
-// Retrieve produce with matching code
+//Retrieve produce with matching code
 func Read(code string) (bool, produce.Produce) {
-	var locatedProduce produce.Produce
-	var located = false
-	var wg sync.WaitGroup
+	var locatedProduce produce.Produce //Create Produce Object
+	var located = false                //Initialize located to false (Indicates if Produce was found)
+	var wg sync.WaitGroup              //Create WaitGroup
 
-	// Start async task to read from db
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for _, produce := range database {
-			if produce.Code == code {
-				locatedProduce = produce
-				located = true
+	//Start async task to read from db
+	wg.Add(1)   //Add one to WaitGroup
+	go func() { //Start GoRoutine
+		defer wg.Done()                    //Execute at the end of this Routine
+		for _, produce := range database { //Loop through database
+			if produce.Code == code { //If Produce code matches provided code
+				locatedProduce = produce //Set locatedProduce to the current produce
+				located = true           //Set located to true
 			}
 		}
 	}()
@@ -54,16 +54,16 @@ func Read(code string) (bool, produce.Produce) {
 	return located, locatedProduce
 }
 
-// Retrieve all produce
+//Retrieve all produce
 func ReadAll() []produce.Produce {
-	var wg sync.WaitGroup
-	var locatedProduce []produce.Produce
+	var locatedProduce []produce.Produce //Create Produce Object List
+	var wg sync.WaitGroup                //Create WaitGroup
 
-	// Start async task to read from db
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		locatedProduce = database
+	//Start async task to read from db
+	wg.Add(1)   //Add one to WaitGroup
+	go func() { //Start GoRoutine
+		defer wg.Done()           //Execute at the end of this Routine
+		locatedProduce = database //Save Produce from database to the produce list
 	}()
 
 	wg.Wait() // Wait for read from the db to complete
@@ -71,18 +71,18 @@ func ReadAll() []produce.Produce {
 	return locatedProduce
 }
 
+//Add produce to the db
 func AddProduce(produce produce.Produce) (bool, produce.Produce) {
+	var added = false     //Initialize added to false (Indicates if Produce was added successfully)
+	var wg sync.WaitGroup //Create WaitGroup
 
-	var added = false
-	var wg sync.WaitGroup
-
-	// Start async task to write to db
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if isCodeUnique(produce.Code) {
-			database = append(database, produce)
-			added = true
+	//Start async task to write to db
+	wg.Add(1)   //Add one to WaitGroup
+	go func() { //Start GoRoutine
+		defer wg.Done()                 //Execute at the end of this Routine
+		if isCodeUnique(produce.Code) { //If code provided does not match any in the DB
+			database = append(database, produce) //Add the produce to the DB
+			added = true                         //Indicate that the produce was added successfully
 		}
 	}()
 
@@ -91,38 +91,26 @@ func AddProduce(produce produce.Produce) (bool, produce.Produce) {
 	return added, produce
 }
 
-func isCodeUnique(code string) bool {
-	var unique = true
-
-	for _, produce := range database {
-		if produce.Code == code {
-			unique = false
-			break
-		}
-	}
-	return unique
-}
-
+//Delete produce from the db
 func DeleteProduce(code string) bool {
-
-	var wg sync.WaitGroup
-	var produceFound bool = false
+	var produceFound = false //Initialize produceFound to false (Indicates if Produce was found)
+	var wg sync.WaitGroup    //Create WaitGroup
 
 	// Start async task to delete from db
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		var index int
-
-		for i, produce := range database {
-			if produce.Code == code {
-				produceFound = true
-				index = i
+	wg.Add(1)   //Add one to WaitGroup
+	go func() { //Start GoRoutine
+		defer wg.Done()                    //Execute at the end of this Routine
+		var index int                      //Create index to track location of produce that matches the code
+		for i, produce := range database { //Loop through database
+			if produce.Code == code { //If provided code matches one found in the DB
+				produceFound = true //Set produceFound to true
+				index = i           //Save index of the produce found
+				break               //End search
 			}
 		}
 
-		if produceFound {
-			copy(database[index:], database[index+1:]) // Shift a[i+1:] left one index
+		if produceFound { //If produce was found, remove it
+			copy(database[index:], database[index+1:]) // Shift database[index+1:] left one index
 			database = database[:len(database)-1]      // Truncate slice
 		}
 	}()
@@ -130,4 +118,17 @@ func DeleteProduce(code string) bool {
 	wg.Wait() // Wait for read from the db to complete
 
 	return produceFound
+}
+
+//Check if the provided code matches any in the DB
+func isCodeUnique(code string) bool {
+	var unique = true //Initialize unique to true(Set to false if code found in DB)
+
+	for _, produce := range database { //Loop through database
+		if produce.Code == code { //If provided code matches one found in the DB
+			unique = false //Set unique to false
+			break          //End search
+		}
+	}
+	return unique
 }
--- src/supermarketService/service.go
+++ src/supermarketService/service.go
@@ -1,186 +1,218 @@
 package service
 
 import (
-	"encoding/json" //For creating JSON responses
-	"github.com/gorilla/mux"
-	"net/http"
-	"regexp"
-	"supermarketDB"
-	"supermarketProduce"
+	"encoding/json"          //For creating JSON responses
+	"github.com/gorilla/mux" //Router that will take requests and decide what should be done (go get github.com/gorilla/mux)
+	"net/http"               //Provides the representation of HTTP requests, responses, and is Responsible for running the server
+	"regexp"                 //Regular Expressions
+	"supermarketDB"          //Provides methods to interact with the "Database"
+	"supermarketProduce"     //Provides the Produce Struct
 )
 
-// Returned on success
+//Strings to be used throughout the service
+const (
+	/*****************Responses************************/
+	success = "Success"
+	failure = "Failure"
+	/*****************Messages*************************/
+	badRequest   = "Bad request"
+	located      = "Produce located"
+	notLocated   = "Could not locate Produce with the provided code"
+	added        = "Produce added"
+	removed      = "Produce removed"
+	notUnique    = "Code is not unique"
+	nameFailVal  = "The Produce name failed validation"
+	codeFailVal  = "The Produce code failed validation"
+	priceFailVal = "The Produce price failed validation"
+	/*****************Regex*****************************/
+	nameRegex  = "^[a-zA-Z0-9 ]+$"
+	codeRegex  = "^([a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{4}$"
+	priceRegex = `^\$[0-9]*\.[0-9]{2}$`
+)
+
+//Reponse object containing produce
+//Result: Status of the request (Success/Failure)
+//Message: Additional information
+//Produce: Produce object
 type ProduceResponseObject struct {
 	Result  string
 	Message string
 	Produce produce.Produce
 }
 
-// Returned on failure
+//Reponse object
+//Result: Status of the request (Success/Failure)
+//Message: Additional information
 type ResponseObject struct {
 	Result  string
 	Message string
 }
 
-// Delete request
+//Delete request
+//Code: Produce code
 type DeleteRequestObject struct {
 	Code string
 }
 
+//GetAllProduce returns a list of produce back to the client
 func GetAllProduce(w http.ResponseWriter, r *http.Request) {
-
-	// Retrieve all produce from db
-	locatedProduce := db.ReadAll()
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(locatedProduce)
+	locatedProduce := db.ReadAll()            //Retrieve all produce from db
+	w.WriteHeader(http.StatusOK)              //Set status to OK
+	json.NewEncoder(w).Encode(locatedProduce) //Return list of produce
 }
 
+//GetProduceByCode returns a produce back to the client
+//Produce returned is based on the provided code
 func GetProduceByCode(w http.ResponseWriter, r *http.Request) {
 
+	//Get code from request
 	code := getCode(r)
 
-	// Retrieve produce with matching code from db
+	//Retrieve produce(locatedProduce) with matching code from db
+	//locatedBoolean is true/false indicating if the produce was found
 	locatedBoolean, locatedProduce := db.Read(code)
 
-	if locatedBoolean {
-		var successObject ProduceResponseObject
-		successObject.Result = "Success"
-		successObject.Message = "Produce located"
-		successObject.Produce = locatedProduce
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(successObject)
-	} else {
-		var failureObject ProduceResponseObject
-		failureObject.Result = "Failure"
-		failureObject.Message = "Could not locate Produce with this code"
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(failureObject)
+	if locatedBoolean { //If produce was found
+		var successObject ProduceResponseObject  //Create Response object
+		successObject.Result = success           //Indicate success
+		successObject.Message = located          //Indicate Produce was located
+		successObject.Produce = locatedProduce   //Set the produce to be returned
+		w.WriteHeader(http.StatusOK)             //Set status to OK
+		json.NewEncoder(w).Encode(successObject) //Return Response object
+	} else { //If Produce was not found
+		var failureObject ProduceResponseObject  //Create Response object
+		failureObject.Result = failure           //Indicate failure
+		failureObject.Message = notLocated       //Indicate Produce was not located
+		w.WriteHeader(http.StatusNotFound)       //Set status to NotFound
+		json.NewEncoder(w).Encode(failureObject) //Return Response object
 	}
 }
 
+//Add Produce to the db
+//The produce added is return upon a successful add
 func AddProduce(w http.ResponseWriter, r *http.Request) {
 
-	decoder := json.NewDecoder(r.Body)
+	decoder := json.NewDecoder(r.Body) //Decode the body
 
-	var incomingProduce produce.Produce
-	err := decoder.Decode(&incomingProduce)
-	if err != nil {
-		panic(err)
-	}
+	var incomingProduce produce.Produce //Create Produce object
+	decoder.Decode(&incomingProduce)    //Save body data to the Produce object
 
-	// Perform validation on incoming Produce
+	//Perform validation on incoming Produce
+	//resultString: Success/Failure
+	//resultMessage: Reason for Success/Failure
+	//resultBoolean: booleaning indicating if passed validation
 	resultString, resultMessage, resultBoolean := validateProduce(incomingProduce)
 
-	if resultBoolean {
-		// Build success object
+	if resultBoolean { //If Passed validation
+		//Add Produce to DB
 		var added, savedProduce = db.AddProduce(incomingProduce)
-		if added {
-			var resultObject ProduceResponseObject
-			resultObject.Result = resultString
-			resultObject.Message = resultMessage
-			resultObject.Produce = savedProduce
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(resultObject)
-		} else {
-			var resultObject ResponseObject
-			resultObject.Result = "Failure"
-			resultObject.Message = "Code is not unique"
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(resultObject)
+		if added { //If added successfully //Return Response object
+			var resultObject ProduceResponseObject  //Create Response object
+			resultObject.Result = resultString      //Set result (Indicate success)
+			resultObject.Message = resultMessage    //Set message (Indicate added successfully)
+			resultObject.Produce = savedProduce     //Set the produce to be returned
+			w.WriteHeader(http.StatusCreated)       //Set status to Created
+			json.NewEncoder(w).Encode(resultObject) //Return Response object
+		} else { //If add was unsuccessful
+			var resultObject ResponseObject         //Create Response object
+			resultObject.Result = failure           //Indicate failure
+			resultObject.Message = notUnique        //Indicate that the code was not unique
+			w.WriteHeader(http.StatusBadRequest)    //Set status to BadRequest
+			json.NewEncoder(w).Encode(resultObject) //Return Response object
 		}
-	} else {
-		// Build failure object
-		var resultObject ResponseObject
-		resultObject.Result = resultString
-		resultObject.Message = resultMessage
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(resultObject)
+	} else { //If Failed validation
+		var resultObject ResponseObject         //Create Response object
+		resultObject.Result = resultString      //Set result (Indicate failure)
+		resultObject.Message = resultMessage    //Set message (Indicate failed validation)
+		w.WriteHeader(http.StatusBadRequest)    //Set status to BadRequest
+		json.NewEncoder(w).Encode(resultObject) //Return Response object
 	}
 }
 
+//Delete Produce from the db
+//The produce deleted is based on the provided code
 func DeleteProduce(w http.ResponseWriter, r *http.Request) {
 
-	decoder := json.NewDecoder(r.Body)
+	decoder := json.NewDecoder(r.Body) //Decode the body
 
-	var incomingDeleteRequest DeleteRequestObject
-	err := decoder.Decode(&incomingDeleteRequest)
-	if err != nil {
-		panic(err)
-	}
+	var incomingDeleteRequest DeleteRequestObject //Create Delete request
+	decoder.Decode(&incomingDeleteRequest)        //Save body to the Delete request
 
-	var code = incomingDeleteRequest.Code
+	var code = incomingDeleteRequest.Code //Save Code from the Delete request
 
-	if validateCode(code) {
-		if db.DeleteProduce(code) {
-			var successObject ResponseObject
-			successObject.Result = "Success"
-			successObject.Message = "Produce removed"
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(successObject)
-		} else {
-			var failureObject ResponseObject
-			failureObject.Result = "Failure"
-			failureObject.Message = "Could not locate Produce with this code"
-			w.WriteHeader(http.StatusNotFound)
-			json.NewEncoder(w).Encode(failureObject)
+	if validateCode(code) { //If the code validates
+		if db.DeleteProduce(code) { //If produce is found and deleted with the provided code
+			var successObject ResponseObject         //Create Response object
+			successObject.Result = success           //Indicate success
+			successObject.Message = removed          //Indicate Produce was removed
+			w.WriteHeader(http.StatusOK)             //Set status to OK
+			json.NewEncoder(w).Encode(successObject) //Return Response object
+		} else { //If deletion fails with the provided code
+			var failureObject ResponseObject         //Create Response object
+			failureObject.Result = failure           //Indicate failure
+			failureObject.Message = notLocated       //Indicate Produce was not located
+			w.WriteHeader(http.StatusNotFound)       //Set status to NotFound
+			json.NewEncoder(w).Encode(failureObject) //Return Response object
 		}
-	} else {
-		var failureObject ResponseObject
-		failureObject.Result = "Failure"
-		failureObject.Message = "The Produce code failed validation"
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(failureObject)
+	} else { //If the code fails validation
+		var failureObject ResponseObject         //Create Response object
+		failureObject.Result = failure           //Indicate failure
+		failureObject.Message = codeFailVal      //Indicate that the provide code failed validation
+		w.WriteHeader(http.StatusBadRequest)     //Set the status to BadRequest
+		json.NewEncoder(w).Encode(failureObject) //Return Response object
 	}
 }
 
+//getCode extracts and returns the code provided in te request
 func getCode(r *http.Request) string {
-	vars := mux.Vars(r) // Get url variables
-	return vars["code"] // Get variable which matching key "code"
+	vars := mux.Vars(r) //Get url variables
+	return vars["code"] //Get variable which matching key "code"
 }
 
+//validateProduce is used to validate Produce objects
 func validateProduce(produce produce.Produce) (string, string, bool) {
 
-	var resultBoolean bool   // true if validation passes
-	var resultString string  // Success/Failure
-	var resultMessage string // Reason for failure
+	var resultBoolean bool   //Is true if validation passes
+	var resultString string  //Indicates Success/Failure
+	var resultMessage string //Indicates the reason for failure
 
 	switch {
-	// If Name fails validation
-	case validateName(produce.Name) == false:
-		resultBoolean = false
-		resultString = "Failure"
-		resultMessage = "The Produce name failed validation"
-	// If Code fails validation
-	case validateCode(produce.Code) == false:
-		resultBoolean = false
-		resultString = "Failure"
-		resultMessage = "The Produce code failed validation"
-	// If Price fails validation
-	case validatePrice(produce.Price) == false:
-		resultBoolean = false
-		resultString = "Failure"
-		resultMessage = "The Produce price failed validation"
-	// If all pass validation
-	default:
-		resultBoolean = true
-		resultString = "Success"
-		resultMessage = "Produce added"
+	case validateName(produce.Name) == false: //If Name fails validation
+		resultBoolean = false       //Set Result to false
+		resultString = failure      //Set the Result string to indicate Failure
+		resultMessage = nameFailVal //Set the message to indicate name failed validation
+	case validateCode(produce.Code) == false: //If Code fails Validation
+		resultBoolean = false       //Set Result to false
+		resultString = failure      //Set the Result string to indicate Failure
+		resultMessage = codeFailVal //Set the message to indicate code failed validation
+	case validatePrice(produce.Price) == false: //If Price fails Validation
+		resultBoolean = false        //Set Result to false
+		resultString = failure       //Set the Result string to indicate Failure
+		resultMessage = priceFailVal //Set the message to indicate price failed validation
+	default: //If all pass validation
+		resultBoolean = true   //Set Result to true
+		resultString = success //Set the Result string to indicate Success
+		resultMessage = added  //Set the message to indicate Produce was added
 	}
 
+	//Return the resultString, resultMessage, and resultBoolean
 	return resultString, resultMessage, resultBoolean
 }
 
+//Returns boolean indicating if name passed validation
 func validateName(name string) bool {
-	// Validate the Name
-	return regexp.MustCompile("^[a-zA-Z0-9 ]+$").MatchString(name)
+	//Validate the Name against regex
+	return regexp.MustCompile(nameRegex).MatchString(name)
 }
 
+//Returns boolean indicating if code passed validation
 func validateCode(code string) bool {
-	// Validate the Code (Assuming the code provided is unique)
-	return regexp.MustCompile("([a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{4}").MatchString(code)
+	//Validate the Code against regex
+	return regexp.MustCompile(codeRegex).MatchString(code)
 }
 
+//Returns boolean indicating if price passed validation
 func validatePrice(price string) bool {
-	// Vaidate the price
-	return regexp.MustCompile(`^\$[0-9]*\.[0-9]{2}$`).MatchString(price)
+	//Vaidate the Price against regex
+	return regexp.MustCompile(priceRegex).MatchString(price)
 }

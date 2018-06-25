8cbbf9f62298e842d396d26d907335746719c178
dkrinke <krinkoid@gmail.com>
2018-06-25 00:05:01 -0400

    Refactored db tests and added comments

--- src/supermarketDB/do_test.go
+++ src/supermarketDB/do_test.go
@@ -3,149 +3,148 @@ package db
 import "testing"
 import "supermarketProduce"
 
-// Test Add Produce
+/********************************Test ReadAll***********************************/
+func TestReadAll(t *testing.T) {
+
+	//Expected result data
+	expectedNameList := []string{"Lettuce", "Peach", "Green Pepper", "Gala Apple"}
+	expectedCodeList := []string{"A12T-4GH7-QPL9-3N4M", "E5T6-9UI3-TH15-QR88", "YRT6-72AS-K736-L4AR", "TQ4C-VV6T-75ZX-1RMR"}
+	expectedPriceList := []string{"$3.46", "$2.99", "$0.79", "$3.59"}
+
+	result := ReadAll() //Read all produce from the db
+
+	for i, produce := range result { //Loop through the resulting produce list
+		if produce.Name != expectedNameList[i] { //Check if name does not match expected
+			t.Errorf("Result[%d] was incorrect, got: %s, want: %s.", i, produce.Name, expectedNameList[i])
+		}
+		if produce.Code != expectedCodeList[i] { //Check if code does not match expected
+			t.Errorf("Result[%d] was incorrect, got: %s, want: %s.", i, produce.Code, expectedCodeList[i])
+		}
+		if produce.Price != expectedPriceList[i] { //Check if price does not match expected
+			t.Errorf("Result[%d] was incorrect, got: %s, want: %s.", i, produce.Price, expectedPriceList[i])
+		}
+	}
+}
+
+/********************************Test Read(Default Data)************************/
+//Test Retrieving Lettuce
+func TestReadLettuce(t *testing.T) {
+	_, result := Read("A12T-4GH7-QPL9-3N4M") //Retrieve Lettuce from db
+
+	if result.Name != "Lettuce" { //Check if name is "Lettuce"
+		t.Errorf("Result was incorrect, got: %s, want: Lettuce.", result.Name)
+	}
+	if result.Code != "A12T-4GH7-QPL9-3N4M" { //Check if resulting code is "A12T-4GH7-QPL9-3N4M"
+		t.Errorf("Result was incorrect, got: %s, want: YRT6-72AS-K736-L4AR.", result.Code)
+	}
+	if result.Price != "$3.46" { //Check if price is "$3.46"
+		t.Errorf("Result was incorrect, got: %s, want: $3.46.", result.Price)
+	}
+}
+
+//Test Retrieving Peach
+func TestReadPeach(t *testing.T) {
+	_, result := Read("E5T6-9UI3-TH15-QR88") //Retrieve Peach from db
+
+	if result.Name != "Peach" { //Check if name is "Peach"
+		t.Errorf("Result was incorrect, got: %s, want: Peach.", result.Name)
+	}
+	if result.Code != "E5T6-9UI3-TH15-QR88" { //Check if resulting code is "E5T6-9UI3-TH15-QR88"
+		t.Errorf("Result was incorrect, got: %s, want: YRT6-72AS-K736-L4AR.", result.Code)
+	}
+	if result.Price != "$2.99" { //Check if price is "$2.99"
+		t.Errorf("Result was incorrect, got: %s, want: $2.99.", result.Price)
+	}
+}
+
+//Test Retrieving Green Pepper
+func TestReadGreenPepper(t *testing.T) {
+	_, result := Read("YRT6-72AS-K736-L4AR") //Retrieve Green Pepper from db
+
+	if result.Name != "Green Pepper" { //Check if name is "Green Pepper"
+		t.Errorf("Result was in0.79correct, got: %s, want: Green Pepper.", result.Name)
+	}
+	if result.Code != "YRT6-72AS-K736-L4AR" { //Check if resulting code is "YRT6-72AS-K736-L4AR"
+		t.Errorf("Result was incorrect, got: %s, want: YRT6-72AS-K736-L4AR.", result.Code)
+	}
+	if result.Price != "$0.79" { //Check if price is "$0.79"
+		t.Errorf("Result was incorrect, got: %s, want: $0.79.", result.Price)
+	}
+}
+
+//Test Retrieving Green Apple
+func TestReadGalaApple(t *testing.T) {
+	_, result := Read("TQ4C-VV6T-75ZX-1RMR") //Retrieve Gala Apple from db
+
+	if result.Name != "Gala Apple" { //Check if name is "Gala Apple"
+		t.Errorf("Result was incorrect, got: %s, want: Gala Apple.", result.Name)
+	}
+	if result.Code != "TQ4C-VV6T-75ZX-1RMR" { //Check if resulting code is "TQ4C-VV6T-75ZX-1RMR"
+		t.Errorf("Result was incorrect, got: %s, want: TQ4C-VV6T-75ZX-1RMR.", result.Code)
+	}
+	if result.Price != "$3.59" { //Check if price is "$3.59"
+		t.Errorf("Result was incorrect, got: %s, want: $3.59.", result.Price)
+	}
+}
+
+/********************************Test AddProduce********************************/
+//Test Successfully adding produce
 func TestAddProduce(t *testing.T) {
 
-	var produce = produce.Produce{Name: "Added Produce", Code: "123A-456B-789C-DEFG", Price: "$99.99"}
-	_, addProduceResult := AddProduce(produce)
+	//Build produce object with valid produce data
+	var produce = produce.Produce{Name: "Golden Apple", Code: "123A-456B-789C-DEFG", Price: "$99.99"}
+	_, addProduceResult := AddProduce(produce) //Add produce and save the returned produce object
 
-	if addProduceResult.Name != produce.Name {
+	if addProduceResult.Name != produce.Name { //Check if name matched the expected name
 		t.Errorf("Result was incorrect, got: %s, want: %s.", addProduceResult.Name, produce.Name)
 	}
-	if addProduceResult.Code != produce.Code {
+	if addProduceResult.Code != produce.Code { //Check if code matched the expected code
 		t.Errorf("Result was incorrect, got: %s, want: %s.", addProduceResult.Code, produce.Code)
 	}
-	if addProduceResult.Price != produce.Price {
+	if addProduceResult.Price != produce.Price { //Check if price matched the expected price
 		t.Errorf("Result was incorrect, got: %s, want: %s.", addProduceResult.Price, produce.Price)
 	}
 }
 
-// Test Delete Produce
-func TestDeleteProduce(t *testing.T) {
+//Test Failure in adding Produce
+//The code is not unique
+func TestAddProduceFailureNonUniqueCode(t *testing.T) {
 
-	firstReadResultBool, _ := Read("123A-456B-789C-DEFG")
-	if firstReadResultBool != true {
+	//Build produce object with valid produce data, but with a non-unique code
+	var produce = produce.Produce{Name: "Golden Apple", Code: "123A-456B-789C-DEFG", Price: "$99.99"}
+	addProduceBoolean, _ := AddProduce(produce) //Add produce and save the returned boolean indicating Success/Failure
+
+	if addProduceBoolean != false { //The boolean should be false indicating that the produce was rejected
+		t.Errorf("Result was incorrect, got: %t, want: false.", addProduceBoolean)
+	}
+}
+
+/********************************Test DeleteProduce*****************************/
+//Test Successfully deleting produce
+func TestDeleteProduceSuccess(t *testing.T) {
+
+	firstReadResultBool, _ := Read("123A-456B-789C-DEFG") //Retreive produce from db
+	if firstReadResultBool != true {                      //Confirm produce exists in db before test
 		t.Errorf("Result was incorrect, got: %t, want: true.", firstReadResultBool)
 	}
 
-	deleteResult := DeleteProduce("123A-456B-789C-DEFG")
-	if deleteResult != true {
-		t.Errorf("Result was incorrect, got: %t, want: true.", deleteResult)
+	deleteResultBool := DeleteProduce("123A-456B-789C-DEFG") //Delete produce and save the resulting boolean
+	if deleteResultBool != true {                            //Check to see if the DeleteProduce function reported success
+		t.Errorf("Result was incorrect, got: %t, want: true.", deleteResultBool)
 	}
 
-	secondReadResultBool, _ := Read("123A-456B-789C-DEFG")
-	if secondReadResultBool != false {
+	secondReadResultBool, _ := Read("123A-456B-789C-DEFG") //Retreive produce from db
+	if secondReadResultBool != false {                     //Confirm produce no longer exists in db
 		t.Errorf("Result was incorrect, got: %t, want: false.", secondReadResultBool)
 	}
 }
 
-// Test Read All
-func TestReadAll(t *testing.T) {
+//Test Failure in deleting Produce
+//The code is not found in the db
+func TestDeleteProduceFailure(t *testing.T) {
 
-	// Test Data
-	testNameList := []string{"Lettuce", "Peach", "Green Pepper", "Gala Apple"}
-	testCodeList := []string{"A12T-4GH7-QPL9-3N4M", "E5T6-9UI3-TH15-QR88", "YRT6-72AS-K736-L4AR", "TQ4C-VV6T-75ZX-1RMR"}
-	testPriceList := []string{"$3.46", "$2.99", "$0.79", "$3.59"}
-
-	result := ReadAll()
-
-	for i, produce := range result {
-		if produce.Name != testNameList[i] {
-			t.Errorf("Result[%d] was incorrect, got: %s, want: %s.", i, produce.Name, testNameList[i])
-		}
-		if produce.Code != testCodeList[i] {
-			t.Errorf("Result[%d] was incorrect, got: %s, want: %s.", i, produce.Code, testCodeList[i])
-		}
-		if produce.Price != testPriceList[i] {
-			t.Errorf("Result[%d] was incorrect, got: %s, want: %s.", i, produce.Price, testPriceList[i])
-		}
-	}
-}
-
-// Test Lettuce
-func TestReadLettuceName(t *testing.T) {
-	_, result := Read("A12T-4GH7-QPL9-3N4M")
-	if result.Name != "Lettuce" {
-		t.Errorf("Result was incorrect, got: %s, want: Lettuce.", result.Name)
-	}
-}
-
-func TestReadLettuceCode(t *testing.T) {
-	_, result := Read("A12T-4GH7-QPL9-3N4M")
-	if result.Code != "A12T-4GH7-QPL9-3N4M" {
-		t.Errorf("Result was incorrect, got: %s, want: YRT6-72AS-K736-L4AR.", result.Code)
-	}
-}
-
-func TestReadLettucePrice(t *testing.T) {
-	_, result := Read("A12T-4GH7-QPL9-3N4M")
-	if result.Price != "$3.46" {
-		t.Errorf("Result was incorrect, got: %s, want: 3.46.", result.Price)
-	}
-}
-
-// Test Peach
-func TestReadPeachName(t *testing.T) {
-	_, result := Read("E5T6-9UI3-TH15-QR88")
-	if result.Name != "Peach" {
-		t.Errorf("Result was incorrect, got: %s, want: Peach.", result.Name)
-	}
-}
-
-func TestReadPeachCode(t *testing.T) {
-	_, result := Read("E5T6-9UI3-TH15-QR88")
-	if result.Code != "E5T6-9UI3-TH15-QR88" {
-		t.Errorf("Result was incorrect, got: %s, want: YRT6-72AS-K736-L4AR.", result.Code)
-	}
-}
-
-func TestReadPeachPrice(t *testing.T) {
-	_, result := Read("E5T6-9UI3-TH15-QR88")
-	if result.Price != "$2.99" {
-		t.Errorf("Result was incorrect, got: %s, want: 2.99.", result.Price)
-	}
-}
-
-// Test Green Pepper
-func TestReadGreenPepperName(t *testing.T) {
-	_, result := Read("YRT6-72AS-K736-L4AR")
-	if result.Name != "Green Pepper" {
-		t.Errorf("Result was in0.79correct, got: %s, want: Green Pepper.", result.Name)
-	}
-}
-
-func TestReadGreenPepperCode(t *testing.T) {
-	_, result := Read("YRT6-72AS-K736-L4AR")
-	if result.Code != "YRT6-72AS-K736-L4AR" {
-		t.Errorf("Result was incorrect, got: %s, want: YRT6-72AS-K736-L4AR.", result.Code)
-	}
-}
-
-func TestReadGreenPepperPrice(t *testing.T) {
-	_, result := Read("YRT6-72AS-K736-L4AR")
-	if result.Price != "$0.79" {
-		t.Errorf("Result was incorrect, got: %s, want: 0.79.", result.Price)
-	}
-}
-
-// Test Green Apple
-func TestReadGalaAppleName(t *testing.T) {
-	_, result := Read("TQ4C-VV6T-75ZX-1RMR")
-	if result.Name != "Gala Apple" {
-		t.Errorf("Result was incorrect, got: %s, want: Gala Apple.", result.Name)
-	}
-}
-
-func TestReadGalaAppleCode(t *testing.T) {
-	_, result := Read("TQ4C-VV6T-75ZX-1RMR")
-	if result.Code != "TQ4C-VV6T-75ZX-1RMR" {
-		t.Errorf("Result was incorrect, got: %s, want: TQ4C-VV6T-75ZX-1RMR.", result.Code)
-	}
-}
-
-func TestReadGalaApplePrice(t *testing.T) {
-	_, result := Read("TQ4C-VV6T-75ZX-1RMR")
-	if result.Price != "$3.59" {
-		t.Errorf("Result was incorrect, got: %s, want: 3.59.", result.Price)
+	deleteResult := DeleteProduce("123A-456B-789C-DEFG") //Delete produce and save the resulting boolean
+	if deleteResult != false {                           //Check to see if the DeleteProduce function reported failure
+		t.Errorf("Result was incorrect, got: %t, want: false.", deleteResult)
 	}
 }
